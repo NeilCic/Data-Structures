5cf64dac3c23367c75d78dedf708a7006f4cc551
NeilCic <50068396+NeilCic@users.noreply.github.com>
2019-05-09 21:17:40 +0300

    Add files via upload

--- WaitablePQ.java
+++ WaitablePQ.java
@@ -58,33 +58,4 @@ public class WaitablePQ<E> {
 	public boolean isEmpty() {
 		return pq.peek() == null;
 	}
-	
-	public static void main(String[] args) {
-		WaitablePQ<Integer> itzik = new WaitablePQ<>((x,y)->y.compareTo(x));
-		
-		itzik.enqueue(1);
-		itzik.enqueue(2);
-		itzik.enqueue(3);
-		itzik.enqueue(4);
-		itzik.enqueue(5);
-		itzik.enqueue(6);
-		itzik.enqueue(7);
-		
-		Thread a = new Thread(() -> { try {
-			while (!itzik.isEmpty()) {System.out.println(Thread.currentThread().getName()+": " + "dequed " +itzik.dequeue());}
-		} catch (InterruptedException e) {
-			System.out.println(Thread.currentThread().getName()+": " + "interrupted");
-		}});
-		
-		Thread b = new Thread(() -> { try {
-			while (!itzik.isEmpty()) {System.out.println(Thread.currentThread().getName()+": " + "dequed " +itzik.dequeue());}
-		} catch (InterruptedException e) {
-			System.out.println(Thread.currentThread().getName()+": " + "interrupted");
-		}});
-		
-		System.out.println("starting threads");
-		
-		a.start();
-		b.start();
-	}
 }
